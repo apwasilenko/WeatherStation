f9f6adae5ddfdaf27fa06485cf5a5d4093a3a18c
Антон Василенко <apwasilenko@gmail.com>
2023-06-01 08:26:17 +0700

    Data obtained from MySQL database
    Получены данные из базы данных MySql

--- mysqlpy.py
+++ mysqlpy.py
@@ -11,9 +11,17 @@ def mysql_py():
                 host="141.8.193.236",
                 user="f0659051_apwasilenko",
                 password="apwasilenko",
+                database="f0659051_apwasilenko",
         ) as connection:
             print(connection)
             print("Соединение уставновлено")
+            select_movies_query = "SELECT * FROM temperatura ORDER BY temperatura.id DESC LIMIT 3"
+            with connection.cursor() as cursor:
+                cursor.execute(select_movies_query)
+                result = cursor.fetchall()
+                print(result)
+                for row in result:
+                    print(row)
     except Error as e:
         print(e)
         print("Соединение не уставновлено")
