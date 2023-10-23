4a1f96a618439ef993a8395515e2ccc80b715a60
APWasilenko-VM <apwasilenko@gmail.com>
2023-10-24 06:13:21 +0700

    Изменена логика рисования подписей и меток.

--- main.py
+++ main.py
@@ -74,24 +74,41 @@ class MainWindow(QWidget):
             qp.setPen(QPen(myColor, sec_thick, cap=Qt.FlatCap))
             qp.drawArc(center_x - radius_circle, center_y - radius_circle, radius_circle * 2, radius_circle * 2, int(1440 - i * 135 / 255 * 16), int(135 / 255 + 1) * 16)
 
-        for i in range(0, int((max_val - min_val)/10)+1):  # рисуем метки на шкале
-            myColor = QColor(0, 0, 0)
-            qp.setPen(QPen(myColor, sec_thick * 1.1, cap=Qt.FlatCap))
-            qp.drawArc(center_x - radius_circle, center_y - radius_circle, radius_circle * 2, radius_circle * 2, int(3584 - i * 270 / int((max_val - min_val)/10) * 16), 2 * 16)
+        for i in range(min_val, max_val): # Выводим подписи
+            if (i%5 ==0) or (i%10 == 0) or (i == min_val) or (i == max_val):
+                myColor = QColor(0, 0, 0)
+                if (i%10 == 0) or (i == min_val) or (i == max_val):
+                    qp.setPen(QPen(myColor, sec_thick * 0.2, cap=Qt.FlatCap))
+                    qp.setFont(QFont("Tahoma", int(radius_circle/10)))
+                else:
+                    qp.setPen(QPen(myColor, sec_thick * 0.1, cap=Qt.FlatCap))
+                    qp.setFont(QFont("Tahoma", int(radius_circle / 15)))
+                size_signature = qp.boundingRect(QRect(), 0, str(i))
+                delta_x = int(size_signature.width()/2)
+                delta_y = int(size_signature.height()/2)
+                position_signature = QPoint(int(center_x + math.cos(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.3 - delta_x),
+                            int(center_y + math.sin(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.3 + delta_y)
+                                      )
+                position1_label = QPoint(int(center_x + math.cos(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.1),
+                            int(center_y + math.sin(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.1)
+                                      )
+                position2_label = QPoint(int(center_x + math.cos(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*0.9),
+                            int(center_y + math.sin(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*0.9)
+                                      )
+                qp.drawLine(position1_label, position2_label)
+                qp.drawText(position_signature, str(i))
 
-        for i in range(0, int((max_val - min_val)/ 10) +1):
-            myColor = QColor(0, 0, 0)
-            qp.setPen(QPen(myColor, sec_thick * 1.1, cap=Qt.FlatCap))
-            qp.setFont(QFont("Tahoma", int(radius_circle/10)))
-            size_label = qp.boundingRect(QRect(), 0, str(min_val + i*10))
-            delta_x = int(size_label.width()/2)
-            delta_y = int(size_label.height()/2)
-            position_label = QPoint(int(center_x + math.cos(math.pi*3/4 + ((min_val + i*10) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.3 - delta_x),
-                        int(center_y + math.sin(math.pi*3/4 + ((min_val + i*10) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.3 + delta_y)
-                                  )
-            qp.drawText(position_label, str(min_val + i*10))
 
-
+            if (i%5 != 0) and (i%10 != 0) and (max_val - min_val < 20):
+                myColor = QColor(0, 0, 0)
+                qp.setPen(QPen(myColor, sec_thick * 0.05, cap=Qt.FlatCap))
+                position1_label = QPoint(int(center_x + math.cos(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.1),
+                            int(center_y + math.sin(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*1.1)
+                                      )
+                position2_label = QPoint(int(center_x + math.cos(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*0.9),
+                            int(center_y + math.sin(math.pi*3/4 + ((i) - min_val)/(max_val-min_val)*6/4*math.pi)*radius_circle*0.9)
+                                      )
+                qp.drawLine(position1_label, position2_label)
 
         # Рисуем стрелку
         points = QPolygon([
@@ -122,10 +139,11 @@ class MainWindow(QWidget):
         myColor = QColor(0, 0, 0)
         qp.setPen(QPen(myColor, sec_thick * 1.1, cap=Qt.FlatCap))
         qp.setFont(QFont("Tahoma", int(radius_circle / 10)))
-        size_label = qp.boundingRect(QRect(), 0, str(cur_val))
-        delta_x = int(size_label.width() / 2)
-        position_label = QPoint(int(center_x - delta_x), int(center_y + radius_circle/2 - delta_y))
-        qp.drawText(position_label, str(cur_val))
+        size_signature = qp.boundingRect(QRect(), 0, str(cur_val))
+        delta_x = int(size_signature.width() / 2)
+        delta_y = int(size_signature.height() / 2)
+        position_signature = QPoint(int(center_x - delta_x), int(center_y + radius_circle/2 - delta_y))
+        qp.drawText(position_signature, str(cur_val))
 
 def mysql_py(col):
     resultat = None
